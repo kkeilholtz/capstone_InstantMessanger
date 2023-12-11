0bc4b371ff10044a32abe2b5d97497c5392e59fa
kkeilholtz <143773724+kkeilholtz@users.noreply.github.com>
2023-12-11 14:32:21 -0600

    [finishes #186624870] [finishes #186624888] [finishes #186600254] this finishes the goal of chat retention, user differentiation, and a button to clear the chat log

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-// Dependencies ////////////////////////////////////////////
+import 'dotenv/config';
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import pkg from 'pg';
+const { Pool } = pkg;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const PORT = process.env.PORT || 5163;
+
+// Postgres configuration ////////////////////////////////// 
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+});
+
+// Maintain a list of connected users //////////////////////
+const users = [];
 
 // Configure the web server ////////////////////////////////
 app
@@ -20,23 +36,68 @@ app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
-  //Socket Connections
+// Socket Connections //////////////////////////////////////
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  //Chat messages
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  // Add user to the list of connected users
+  const userName = socket.handshake.query.name;
+  users.push(userName);
+
+  // Show previous messages from the database, displaying an error if there is one
+  pool.query('SELECT * FROM messages ORDER BY timestamp ASC', (error, result) => {
+    if (error) {
+      console.error('Error pulling messages from DB. ERROR:', error);
+      return;
+    }
+    result.rows.forEach(row => {
+      io.to(socket.id).emit('chat message', {
+        username: row.username,
+        message: row.message,
+      });
+    });
   });
 
-  //Disconnection
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  // Chat messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', {
+      username: userName,
+      message: msg,
+    });
+
+    // Messages to DB
+    const insert = 'INSERT INTO messages (username, message) VALUES ($1, $2)';
+    const values = [userName, msg];
+    pool.query(insert, values, (error) => {
+      if (error) {
+        console.error('Error inserting messages to DB. ERROR: ', error);
+      }
+    });
+  });
+
+// Clear chat event
+socket.on('clear chat', () => {
+  pool.query('TRUNCATE TABLE messages', (error) => {
+    if (error) {
+      console.error('Error truncating messages:', error);
+      return;
+    }
+    io.emit('cleared chat');
   });
 });
 
+  // Disconnection
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+
+    // Remove user from the list of connected users
+    const index = users.indexOf(userName);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+  });
+});
 // Ready for browsers to connect ///////////////////////////
-const PORT = process.env.PORT || 5163
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
